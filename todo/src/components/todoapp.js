7755fadd6db689d40e5ee1eeef4d40b9a8a9a941
olaerjunerey <olaerjunerey@yahoo.com>
2016-04-25 18:04:56 +0800

    2nd commit

--- todo/src/components/todoapp.js
+++ todo/src/components/todoapp.js
@@ -9,27 +9,55 @@ export default  class TodoApp extends React.Component{
         super(props);
         
         this.state={
-            name: 'student'
-        }
-     this.onChange=this.onChange.bind(this);
+            mytodo:'',
+            list:[ 
+            ]
+        };
+        
     }
+    
     onChange(e){
         this.setState({
-            name:e.target.value
+            mytodo:e.target.value
         })
     }
-
-
+    
+    onAddItem(){
+        var listx = this.state.list;
+        listx.push(this.state.mytodo)
+        
+        this.setState({
+            list:listx
+        })
+    }
+    
+    onKeyUp(e){
+            if( e.key==='Enter'){
+                let  tmp = this.state.list;
+                tmp.push(this.state.mytodo);
+                this.setState({
+                    list:tmp,
+                    mytodo:''
+                })
+            }
+        }
+    
     render(){
+        let items=[];
+        for(var i=0;i<this.state.list.length;i++){
+                items.push(<li>{this.state.list[i]}</li>)
+        }
 
         return (
             <div>
-            <h1><font color="pink">Welcome to ReactJS {this.state.name} </font></h1>
-            <strong>Enter your name</strong><br />
-            <form>
-            <input type="text" onChange={this.onChange.bind(this)} />
-            </form>
+            <h1>My Todo List</h1>
+            <input type="text" onChange={this.onChange.bind(this)} 
+                value={this.state.mytodo}
+                onKeyUp={this.onKeyUp.bind(this)}/>
+            <ol>
+            {items}
+            </ol>
         </div>
-    )
+        )
     }
 }
